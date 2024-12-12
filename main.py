06b51ef98d24b7f578dd4fceff9d66dcc0a83ef5
mateusbarbosa01 <mateus.bakerivard@gmail.com>
2024-12-12 09:43:31 -0300

    added single image-to-text functionality.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ sys.path.append('C:/Users/mateu/PycharmProjects/book_scanner')
 
 from ocr import *
 
-
 #print(extract_text_from_image('C:/Users/mateu/PycharmProjects/book_scanner/data/book_spines/1-side.jpg'))
 
 
